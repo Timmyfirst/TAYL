6573cfdd0cc1468d60cbcdbca9c4f0448f15f490
ayolia18 <ayolia18@hotmail.fr>
2018-01-29 15:44:06 +0100

    create file json for phpLoc

--- app/Http/Controllers/PhpLocController.php
+++ app/Http/Controllers/PhpLocController.php
@@ -14,11 +14,31 @@ class PhpLocController extends Controller
 
         /*get the date to put it at the end of the log file name*/
         $date =  date('Y_m_d_G-i-s');
-        $nameLogFile= 'logPhpLoc'.$date.'.txt';
+        $nameLogFile= 'logPhpLoc'.$date;
         $pathStorage = public_path() . "/storage/";
 
         /*execute a command to execute "phploc" and send the result to a log file*/
-        shell_exec( 'cd '.$pathStorage .' && phploc project > logProject/'.$nameLogFile);
+        shell_exec( 'cd '.$pathStorage .' && phploc project > logProject/'.$nameLogFile.'.txt');
+        shell_exec( 'cd '.$pathStorage .' && phploc project --log-xml logProject/'.$nameLogFile.'.xml');
+
+
+        /*create file Xml*/
+        $logXml = simplexml_load_file($pathStorage.'logProject/'.$nameLogFile.'.xml');
+
+        /*convert file xml in json*/
+        $jsonTab = json_encode($logXml);
+
+        /*write in file json*/
+        $fp=fopen($pathStorage.'/logProject/'.$nameLogFile.'.json','w');
+        if ($fp==false)
+        {echo 'echec';
+
+        }
+        else {
+            fputs($fp, '[' . $jsonTab . ']');
+            fclose($fp);
+        }
+
 
         /*Insert in log table*/
         $logTest = new LogTest;
